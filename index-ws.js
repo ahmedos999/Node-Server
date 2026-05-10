2da3a20d7c3eca7604b2911ecf4a5f8d425b9c96
Ahmed Sheikh <ahmedos099912@gmail.com>
2026-05-10 13:04:01 +0400

    Adding db loggin

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,16 @@ server.listen(3000, function () {
   console.log("server started on port 3000");
 });
 
+// to listen to server process kill and close connections
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 // Begin websocket
 const WebSocketServer = require("ws").Server;
 
@@ -28,6 +38,9 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server");
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numOfClients}, datetime('now'))`);
+
   ws.on("close", function close() {
     wss.broadcast(`current visitors ${numOfClients}`);
     console.log("A Client has disconnected");
@@ -39,3 +52,30 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+// Database
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+      CREATE TABLE visitors (
+        count INTEGER,
+        time TEXT
+      )
+    `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+
+  console.log("shuting down");
+  db.close();
+}
